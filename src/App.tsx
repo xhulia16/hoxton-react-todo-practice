ef850d271af865f3b86c67f79e155b5e077f84da
XHULIA PRISHKA <xh.prishka@gmail.com>
2022-08-15 20:28:03 +0200

    create a new todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,57 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 
-import './App.css'
-import { Todo } from './types'
+import "./App.css";
+import { Todo } from "./types";
 
 function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
 
-  const [todos, setTodos]=useState<Todo[]>([])
-
-  useEffect(()=>{
-    fetch('http://localhost:4000/todos')
-    .then(resp=>resp.json()
-    .then(todosFromServer=> setTodos(todosFromServer)))
-  },[])
+  useEffect(() => {
+    fetch("http://localhost:4000/todos").then((resp) =>
+      resp.json().then((todosFromServer) => setTodos(todosFromServer))
+    );
+  }, []);
   return (
     <div className="App">
-     <header className="header">
-      <h1>ToDo List✨</h1>
-      <input placeholder='Search a todo...'>
-  
-      </input>
+      <header className="header">
+        <h1>ToDo List✨</h1>
+        <input placeholder="Search a todo..."></input>
       </header>
-     <main className="main">
-      <ul className="todo-list">
-        {todos.map(item=>
-        <li className={item.completed? 'completed': 'not-completed'}>{item.content}</li>
-          )}
-      </ul>
-      <form>
-        <input placeholder="add a todo"></input>
-        <button>Submit</button>
-      </form>
-     </main>
+      <main className="main">
+        <ul className="todo-list">
+          {todos.map((item) => (
+            <li className={item.completed ? "completed" : "not-completed"}>
+              {item.content}
+            </li>
+          ))}
+        </ul>
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+            let newTodo = {
+              content: event.target.newTodo.value,
+              completed: false,
+            };
+            setTodos([...todos, newTodo]);
+            fetch('http://localhost:4000/todos', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify({
+              content: event.target.newTodo.value,
+              completed: false,
+              })
+            })
+            event.target.reset();
+          }}
+        >
+          <input placeholder="add a todo" name="newTodo"></input>
+          <button>Submit</button>
+        </form>
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
